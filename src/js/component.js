4a2024443de8de3923e3acd69b6825da91de3868
Andriy Lytvyn <andriy@devsalsa.com>
2023-12-21 22:57:35 +0200

    add 2 block

--- src/js/component.js
+++ src/js/component.js
@@ -59,6 +59,24 @@ $( document ).ready(function() {
     $('.slider-estate').slick('slickNext');
   })
        
+  $('.slider-estate-small').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    variableWidth: true,
+    prevArrow: `<button class="slider-prev flex items-center justify-center shadow-xl w-11 h-11 rounded-full cursor-pointer text-[#5E5E5E] hover:text-white hover:bg-[#FF6B2A] transition absolute top-48 lg:left-2 xl:-left-9 z-20 bg-white">
+    <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M11.5785 12.1072L16.5285 7.15719L15.1145 5.74319L8.75049 12.1072L15.1145 18.4712L16.5285 17.0572L11.5785 12.1072Z" fill="currentColor"></path>
+    </svg>
+  </button>`,
+    nextArrow: `<button class="slider-next flex items-center justify-center shadow-xl w-11 h-11 rounded-full cursor-pointer text-[#5E5E5E] hover:text-white hover:bg-[#FF6B2A] transition absolute top-48 right-2 z-20 bg-white">
+    <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M13.4215 12.1072L8.47152 7.15719L9.88551 5.74319L16.2495 12.1072L9.88551 18.4712L8.47151 17.0572L13.4215 12.1072Z" fill="currentColor"></path>
+    </svg>
+  </button>`,
+  });
+  
 }) 
 
 
