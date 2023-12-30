ff658f3e5414af324644386c5cc99904675e7e6a
Andriy Lytvyn <andriy@devsalsa.com>
2023-12-31 01:47:36 +0200

    add company pages

--- src/js/component.js
+++ src/js/component.js
@@ -745,4 +745,46 @@ $( document ).ready(function() {
     }
   };
 
+  $('.object-tab-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    lazyLoad: 'ondemand',
+    prevArrow: `<button class="slider-prev flex items-center justify-center shadow-xl w-11 h-11 rounded-full cursor-pointer text-primary_gray hover:text-white hover:bg-primary transition absolute top-1/2 -translate-y-1/2 -left-5 lg:-left-[63px] z-20 bg-white">
+    <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M11.5785 12.1072L16.5285 7.15719L15.1145 5.74319L8.75049 12.1072L15.1145 18.4712L16.5285 17.0572L11.5785 12.1072Z" fill="currentColor"></path>
+    </svg>
+  </button>`,
+    nextArrow: `<button class="slider-next flex items-center justify-center shadow-xl w-11 h-11 rounded-full cursor-pointer text-primary_gray hover:text-white hover:bg-primary transition absolute top-1/2 -translate-y-1/2 -right-5 lg:-right-[63px] z-20 bg-white">
+    <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M13.4215 12.1072L8.47152 7.15719L9.88551 5.74319L16.2495 12.1072L9.88551 18.4712L8.47151 17.0572L13.4215 12.1072Z" fill="currentColor"></path>
+    </svg>
+  </button>`,
+    responsive: [
+      {
+        breakpoint: 960,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          variableWidth: false,
+          centerMode: false,
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.tabs-company li a').click(function(){
+    $('.object-tab-slider').slick('refresh');
+
+    $('.tabs-company li a').removeClass('!text-primary');
+    $(this).addClass('!text-primary')
+  })
+
 }) 
