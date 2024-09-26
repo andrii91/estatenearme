67fb07171696ad107843b5252ae3ca7b9848d8ab
Andriy Lytvyn <andriy@devsalsa.com>
2024-09-26 23:16:16 +0300

    add lending page

--- dist/js/app.js
+++ dist/js/app.js
@@ -974,5 +974,34 @@ $('.slider-similar-slider').slick({
   ]
 });
 
+  $('.slider-logos').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToScroll: 1,
+    slidesToShow: 6,
+   
+    lazyLoad: 'ondemand',
+    prevArrow: `<button class="slider-prev flex items-center justify-center shadow-xl w-11 h-11 rounded cursor-pointer text-primary_gray hover:text-white hover:bg-primary transition absolute top-1/2 -translate-y-1/2 left-0 2xl:-left-[63px] z-20 bg-white">
+    <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M11.5785 12.1072L16.5285 7.15719L15.1145 5.74319L8.75049 12.1072L15.1145 18.4712L16.5285 17.0572L11.5785 12.1072Z" fill="currentColor"></path>
+    </svg>
+  </button>`,
+    nextArrow: `<button class="slider-next flex items-center justify-center shadow-xl w-11 h-11 rounded cursor-pointer text-primary_gray hover:text-white hover:bg-primary transition absolute top-1/2 -translate-y-1/2 right-0 2xl:-right-[63px] z-20 bg-white">
+    <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M13.4215 12.1072L8.47152 7.15719L9.88551 5.74319L16.2495 12.1072L9.88551 18.4712L8.47151 17.0572L13.4215 12.1072Z" fill="currentColor"></path>
+    </svg>
+  </button>`,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        variableWidth: true,
+        arrows: false
+      }
+    },
+  ]
+  });
 
 }) 
